868cd28ca4063c5808aff0372adb142ffba00644
Bader, Samuel <samuel.bader@intel.com>
2023-11-08 18:22:41 -0800

    Made Smith curves grey

--- src/bokeh_smith/core.py
+++ src/bokeh_smith/core.py
@@ -22,14 +22,14 @@ def smith_chart(
 
     # Curves of constant a=Re{Γ} are circles with center x0,y0 = a/(a+1),0 with radius 1/(a+1)
     a = np.asarray(a_values)
-    p.ellipse(x=a / (a + 1), y=0, width=2 / (a + 1), height=2 / (a + 1), fill_color=None, line_color='black',
+    p.ellipse(x=a / (a + 1), y=0, width=2 / (a + 1), height=2 / (a + 1), fill_color=None, line_color='grey',
                  line_width=.5)
 
     # Curves of constant b=Im{Γ} are circles with center x0,y0 = 1,1/b with radius 1/b
     # And for b=0, that's just a horizontal line at y=0
     b = np.asarray(b_values)
-    p.ellipse(x=1, y=1 / b, width=2 / b, height=2 / b, fill_color=None, line_color='black', line_width=.5)
-    p.line([-1, 1], [0, 0], line_color='black', line_width=.5)
+    p.ellipse(x=1, y=1 / b, width=2 / b, height=2 / b, fill_color=None, line_color='grey', line_width=.5)
+    p.line([-1, 1], [0, 0], line_color='grey', line_width=.5)
 
     # Cover up the area above and below the unit circle
     x = np.linspace(-1, 1, 30)
@@ -37,7 +37,7 @@ def smith_chart(
     p.varea(x=(x - .5) * 1.01 + .5, y1=np.sqrt(1 - x ** 2) + .01, y2=2, fill_color='white')
 
     # Put an x in the center
-    p.x(0, 0, line_color='black')
+    p.x(0, 0, line_color='grey')
 
     # Turn off axis and cartesian grid
     p.xaxis.visible = False
